2d9b0045fef8cc27c4ff1e7bd4a1d9a03c91074f
toushika <toushika111@gmail.com>
2022-03-14 22:18:29 +0600

    remove unused code

--- src/main/java/com/enigma/rediscrudoperation/model/EmployeeId.java
+++ src/main/java/com/enigma/rediscrudoperation/model/EmployeeId.java
@@ -4,13 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class EmployeeId implements Serializable {
-    private static final long serialVersionUID = -7817224776021728682L;
-
+public class EmployeeId {
     private Integer empId;
 }
--- src/main/java/com/enigma/rediscrudoperation/model/EmployeeMap.java
+++ src/main/java/com/enigma/rediscrudoperation/model/EmployeeMap.java
@@ -1,7 +1,14 @@
 package com.enigma.rediscrudoperation.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeMap {
     private Map<Integer, Employee> empIdEmployeeMap;
 }
